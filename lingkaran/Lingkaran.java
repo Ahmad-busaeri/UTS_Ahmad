c3e84cab382f42fa713f3a162a8be7733b5d44c4
eyO18 <106310176+Ahmad-busaeri@users.noreply.github.com>
2022-11-17 14:35:16 +0700

    uts

---
+++ lingkaran/Lingkaran.java
@@ -0,0 +1,18 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package uts.lingkaran;
+
+/**
+ *
+ * @author user
+ */
+public interface Lingkaran {
+    int PANJANG = 12;
+    int LEBAR = 7;
+    
+    void hitungLuas();
+    void tampilkanLuas();
+}
